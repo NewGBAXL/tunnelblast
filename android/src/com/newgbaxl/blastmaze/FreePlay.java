169b37bd8452469c30e2eee8976bf9f810c415a7
Brain_Glitch <draqonhearted@gmail.com>
2022-12-05 21:22:52 -0500

    Full points achievable.

--- android/src/com/newgbaxl/blastmaze/FreePlay.java
+++ android/src/com/newgbaxl/blastmaze/FreePlay.java
@@ -157,7 +157,7 @@ public class FreePlay extends Fragment {
         i3.putExtra("Timer", GlobalVars.timer);
         i3.putExtra("Bombs", GlobalVars.timer);
         i3.putExtra("Walls", GlobalVars.walls);
-        i3.putExtra("Enemies",GlobalVars.enemies);
+        i3.putExtra("Enemies", GlobalVars.enemies);
         startActivity(i3);
     }
 
