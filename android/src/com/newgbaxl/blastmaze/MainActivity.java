2cc1be6d74e0c2638a58a4b61b01477ef98cbbec
Nohbdy <NicolasCarty03@gmail.com>
2022-12-05 20:46:37 -0500

    Added FAB back (for Eric)

--- android/src/com/newgbaxl/blastmaze/MainActivity.java
+++ android/src/com/newgbaxl/blastmaze/MainActivity.java
@@ -48,6 +48,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         mediaPlayer = new MediaPlayer();
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
