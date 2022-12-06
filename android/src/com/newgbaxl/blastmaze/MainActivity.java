049fd8028f7cbecabd0933ef5741ec02165e964d
Brain_Glitch <draqonhearted@gmail.com>
2022-12-05 20:59:29 -0500

    Full points achievable.

--- android/src/com/newgbaxl/blastmaze/MainActivity.java
+++ android/src/com/newgbaxl/blastmaze/MainActivity.java
@@ -51,8 +51,7 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                helpSelect(view);
             }
         });
     }
